021b930606fe82c121ff1724ace2a91e9bff11c3
neel-dabhi <yoursdabhineelkanth@gmail.com>
2025-06-24 13:10:03 -0300

    Completed Heaps 1

---
+++ KthLargest.java
@@ -0,0 +1,16 @@
+// Time complexity O(n log(k) )
+// Space complexity O(k)
+// Runs on LeetCode Yes
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+
+        for(int i=0; i< nums.length; i++){
+            q.add(nums[i]);
+            if(q.size() > k){
+                q.poll();
+            }
+        }
+        return q.poll();
+    }
+}
